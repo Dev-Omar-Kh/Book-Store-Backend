f34443f53b1dbfcd047dbb81d567e5e2e1b90049
Dev-Omar-Kh <162002858+Dev-Omar-Kh@users.noreply.github.com>
2024-10-13 14:30:27 +0300

    Update auth.controllers.js

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -43,7 +43,7 @@ export const emailSignIn = async (req, res, next) => {
 		res.cookie("access_token", token, {
 			httpOnly: true,
 			maxAge: 7 * 60 * 60 * 1000,
-			secure: true,
+			secure: process.env.NODE_ENV === 'production'
     			sameSite: 'None'
 		});
 		return res.status(200).json({
