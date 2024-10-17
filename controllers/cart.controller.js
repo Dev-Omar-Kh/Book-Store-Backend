e2fbe8a5d18304c365e5532854dac57bec1ebd71
Mohamed0Hisham <hishamsovereign321@gmail.com>
2024-10-17 15:43:24 +0300

    cart routes enhanced

---
+++ controllers/cart.controller.js
@@ -0,0 +1,132 @@
+import express from "express";
+import Cart from "../models/cart.model.js";
+import { errorHandler } from "../middleware/errorHandler.js";
+
+export const addProductToCart = async (req, res, next) => {
+	try {
+		const { bookId } = req.body;
+		const currentUser = req.user.id;
+
+		const isProductAvailable = await Cart.findOne({ bookId });
+
+		if (isProductAvailable) {
+			return next(errorHandler(400, "book already in the cart"));
+		}
+
+		const payload = {
+			bookId: bookId,
+			quantity: 1,
+			userId: currentUser,
+		};
+
+		const newAddToCart = new Cart(payload);
+		const saveProduct = await newAddToCart.save();
+
+		return res.json({
+			data: saveProduct,
+			message: "Book Added to Cart",
+			success: true,
+		});
+	} catch (err) {
+		return next(
+			errorHandler(400, err?.message || "bad request to add to the cart")
+		);
+	}
+};
+
+export const getUserCart = async (req, res, next) => {
+	try {
+		const currentUser = req.user.id;
+
+		const allProduct = await Cart.find({
+			userId: currentUser,
+		}).populate("bookId");
+
+		res.json({
+			data: allProduct,
+			success: true,
+			message: "all books fetched",
+		});
+	} catch (err) {
+		return next(
+			errorHandler(
+				404,
+				err?.message || " couldn't fetch the cart elements"
+			)
+		);
+	}
+};
+
+export const getUserCartCount = async (req, res, next) => {
+	try {
+		const userId = req.user.id;
+		const count = await Cart.countDocuments({
+			userId: userId,
+		});
+
+		res.json({
+			data: {
+				count: count,
+			},
+			message: "count of cart products fetched",
+			success: true,
+		});
+	} catch (error) {
+		return next(
+			errorHandler(
+				404,
+				err?.message || " couldn't fetch the cart elements count"
+			)
+		);
+	}
+};
+
+export const deleteProductFromCart = async (req, res, next) => {
+	try {
+		const { cartProductId } = req.body;
+
+		const deleteProduct = await Cart.deleteOne({
+			_id: cartProductId,
+		});
+
+		res.json({
+			message: "Book deleted from cart",
+			success: true,
+			data: deleteProduct,
+		});
+	} catch (err) {
+		return next(
+			errorHandler(
+				404,
+				err?.message || " couldn't delete the cart element"
+			)
+		);
+	}
+};
+
+export const updateProductCountInCart = async (req, res, next) => {
+	try {
+		const { cartProductId } = req.body;
+
+		const { quantity } = req.body;
+
+		const updateProduct = await Cart.updateOne(
+			{ _id: cartProductId },
+			{
+				...(quantity && { quantity: quantity }),
+			}
+		);
+
+		res.json({
+			message: "Book quantity Updated",
+			success: true,
+		});
+	} catch (err) {
+		return next(
+			errorHandler(
+				404,
+				err?.message || " couldn't update the cart element"
+			)
+		);
+	}
+};
--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,135 +1,22 @@
 import express from "express";
-import Cart from "../models/cart.model.js";
-import { errorHandler } from "../middleware/errorHandler.js";
+
+import {
+	addProductToCart,
+	deleteProductFromCart,
+	getUserCart,
+	getUserCartCount,
+	updateProductCountInCart,
+} from "../controllers/cart.controller.js";
 const router = express.Router();
 
-router.post("/add", async (req, res, next) => {
-	try {
-		const { bookId } = req.body;
-		const currentUser = req.user.id;
+router.post("/add", addProductToCart);
 
-		const isProductAvailable = await Cart.findOne({ bookId });
+router.get("/allProducts", getUserCart);
 
-		if (isProductAvailable) {
-			return next(errorHandler(400, "book already in the cart"));
-		}
+router.get("/count-cart-elements", getUserCartCount);
 
-		const payload = {
-			bookId: bookId,
-			quantity: 1,
-			userId: currentUser,
-		};
+router.delete("/delete", deleteProductFromCart);
 
-		const newAddToCart = new Cart(payload);
-		const saveProduct = await newAddToCart.save();
-
-		return res.json({
-			data: saveProduct,
-			message: "Book Added to Cart",
-			success: true,
-		});
-	} catch (err) {
-		return next(
-			errorHandler(400, err?.message || "bad request to add to the cart")
-		);
-	}
-});
-
-router.get("/allProducts", async (req, res, next) => {
-	try {
-		const currentUser = req.user.id;
-
-		const allProduct = await Cart.find({
-			userId: currentUser,
-		}).populate("bookId");
-
-		res.json({
-			data: allProduct,
-			success: true,
-			message: "all books fetched",
-		});
-	} catch (err) {
-		return next(
-			errorHandler(
-				404,
-				err?.message || " couldn't fetch the cart elements"
-			)
-		);
-	}
-});
-
-router.get("/count-cart-elements", async (req, res, next) => {
-	try {
-		const userId = req.user.id;
-		const count = await Cart.countDocuments({
-			userId: userId,
-		});
-
-		res.json({
-			data: {
-				count: count,
-			},
-			message: "count of cart products fetched",
-			success: true,
-		});
-	} catch (error) {
-		return next(
-			errorHandler(
-				404,
-				err?.message || " couldn't fetch the cart elements count"
-			)
-		);
-	}
-});
-
-router.delete("/delete", async (req, res, next) => {
-	try {
-		const { cartProductId } = req.body;
-
-		const deleteProduct = await Cart.deleteOne({
-			_id: cartProductId,
-		});
-
-		res.json({
-			message: "Book deleted from cart",
-			success: true,
-			data: deleteProduct,
-		});
-	} catch (err) {
-		return next(
-			errorHandler(
-				404,
-				err?.message || " couldn't delete the cart element"
-			)
-		);
-	}
-});
-
-router.patch("/update", async (req, res, next) => {
-	try {
-		const { cartProductId } = req.body;
-
-		const { quantity } = req.body;
-
-		const updateProduct = await Cart.updateOne(
-			{ _id: cartProductId },
-			{
-				...(quantity && { quantity: quantity }),
-			}
-		);
-
-		res.json({
-			message: "Book quantity Updated",
-			success: true,
-		});
-	} catch (err) {
-		return next(
-			errorHandler(
-				404,
-				err?.message || " couldn't update the cart element"
-			)
-		);
-	}
-});
+router.patch("/update", updateProductCountInCart);
 
 export default router;
